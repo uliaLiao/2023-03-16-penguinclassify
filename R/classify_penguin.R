d9d2ebff7a38713f7ba19381fc62545ef23711b2
Jiaying Liao <jiaying.liao@qq.com>
2023-03-23 10:04:20 -0700

    export

--- R/classify_penguin.R
+++ R/classify_penguin.R
@@ -1,10 +1,5 @@
-# library(testthat)
-
+#' @export
 classify_penguin <- function(bill_length, flipper_length) {
-#
-#   stopifnot(is_numeric(bill_length) && is_numeric(flipper_length))
-#   stopifnot(bill_length>0 && flipper_length>0)
-
   if (flipper_length>205) {
     return("Gentoo")
   } else {
@@ -15,7 +10,4 @@ classify_penguin <- function(bill_length, flipper_length) {
     }
   }
 }
-# expect_equal(classify_penguin(40,210), "Gentoo")
-# expect_equal(classify_penguin(40,200), "Adelle")
-# expect_error(classify_penguin("zzz",10))
-# expect_error(classify_penguin(0,-5))
+
